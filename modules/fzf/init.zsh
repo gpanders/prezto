a5d3b61c7dbe1187a7b3aa861aed9906a0a5f1fc
Greg Anders <ganders@utexas.edu>
2017-06-23 14:17:31 -0500

    Add fzf options to zpreztorc

--- modules/fzf/init.zsh
+++ modules/fzf/init.zsh
@@ -6,11 +6,37 @@
 #
 
 # Return if requirements are not found.
-if [ ! -f ~/.fzf.zsh ]; then
+if (( ! $+commands[fzf] )); then
   return 1
 fi
 
-source ~/.fzf.zsh
+if zstyle -t ':prezto:module:fzf' key-bindings; then
+  source "${0:h}/external/shell/key-bindings.zsh"
+fi
+
+if zstyle -t ':prezto:module:fzf' completion; then
+  [[ $- == *i* ]] && source "${0:h}/external/shell/completion.zsh" 2>/dev/null
+fi
+
+# Set height of fzf results
+zstyle -s ':prezto:module:fzf' height FZF_HEIGHT
+
+# Open fzf in a tmux pane if using tmux
+if zstyle -t ':prezto:module:fzf' tmux && [ -n "$TMUX_PANE" ]; then
+  export FZF_TMUX=1
+  export FZF_TMUX_HEIGHT=${FZF_HEIGHT:-40%}
+  alias fzf="fzf-tmux -d${FZF_TMUX_HEIGHT}"
+else
+  export FZF_TMUX=0
+  if [ ! -z "$FZF_HEIGHT" ]; then
+    export FZF_DEFAULT_OPTS="--height ${FZF_HEIGHT} --reverse"
+  fi
+fi
+
+__fzf_prog() {
+  [ -n "$TMUX_PANE" ] && [ "${FZF_TMUX:-0}" != 0 ] && [ ${LINES:-40} -gt 15 ] \
+    && echo "fzf-tmux -d${FZF_TMUX_HEIGHT}" || echo "fzf"
+}
 
 # Use ag if available
 if (( $+commands[ag] )); then
@@ -26,17 +52,18 @@ fi
 # to $FZF_DEFAULT_COMMAND
 export FZF_DEFAULT_COMMAND="(git ls-tree -r --name-only HEAD || $FZF_DEFAULT_COMMAND) 2>/dev/null"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_DEFAULT_OPTS="--inline-info"
+export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --inline-info"
 
 # If fasd is loaded, pipe output to fzf
 # Note that the `fzf-tmux` command works regardless of whether or not the user is
 # in a tmux session. If no tmux session is detected, it acts just like `fzf`
 if zstyle -t ':prezto:module:fasd' loaded; then
   unalias j 2>/dev/null
-  fzf_cd() {
-    local dir
-    dir="$(fasd -Rdl "$1" | fzf-tmux -1 -0 --no-sort +m)" && cd "${dir}" || return 1
+  __fzf_cd() {
+    local dir fzf
+    fzf=$(__fzf_prog)
+    dir="$(fasd -Rdl "$1" | ${=fzf} -1 -0 --no-sort +m)" && cd "${dir}" || return 1
   }
-  alias j='fzf_cd'
+  alias j='__fzf_cd'
 fi
 
